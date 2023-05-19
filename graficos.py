650d0dca5e6e7ca6a464c861c50596f5a702996b
Marcos <marcosmontemarano@hotmail.com>
2023-05-19 17:13:00 -0300

    extraje columnas desde el df

--- graficos.py
+++ graficos.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from suavizado import suavizado
 
 # Lee el archivo Excel y crea un DataFrame
 df_0 = pd.read_excel('RESPUESTA_FRECUENCIA_0.xlsx')
 
-#hola como andas?  bien
+# Extrae columna de frecuencias
+frec_0 = df_0.iloc[:,[0]]
+
+# Extrae columna de magnitud en dB
+db_0 = df_0.iloc[:,[1]]
+
+# Suavizado con función externa
+
+suavizado_0 = suavizado(frec_0,db_0,3)
