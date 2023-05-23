df1735f1b00970ed6424420b5f8412e1e0f848ae
Marcos <marcosmontemarano@hotmail.com>
2023-05-23 16:05:53 -0300

    modificaciones en excel

--- graficos.py
+++ graficos.py
@@ -19,9 +19,10 @@ db_45 = df_45.iloc[1:,[1]]
 db_90 = df_90.iloc[1:,[1]]
 
 # Suavizado con función externa
-#suavizado_0 = suavizado(frec_0,db_0,3)
+suavizado_0 = suavizado(df_0['Unnamed: 0'], df_0['rta freq 0°'], 3)
 
 #Plot Rta. Fcia. 0°
+plt.figure(1)
 plt.plot(frec_0,db_0)
 plt.semilogx()
 plt.xlabel("Frequency [Hz]")
@@ -30,17 +31,19 @@ plt.title("Frequency Response 0°")
 plt.ylim([-2,9])
 
 #Plot Rta. Fcia. 45°
+plt.figure(2)
 plt.plot(frec_45,db_45)
 plt.semilogx()
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude [dB]")
-plt.title("Frequency Response 0°")
+plt.title("Frequency Response 45°")
 plt.ylim([-2,9])
 
 #Plot Rta. Fcia. 90°
+plt.figure(3)
 plt.plot(frec_90,db_90)
 plt.semilogx()
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude [dB]")
-plt.title("Frequency Response 0°")
+plt.title("Frequency Response 90°")
 plt.ylim([-2,9])
