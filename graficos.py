f5866b1a24dfc0418fdfe3f254837b80de241c01
Marcos Montemarano <marcosmontemarano@hotmail.com>
2023-05-27 10:21:17 -0300

    ploteo suavizado

--- graficos.py
+++ graficos.py
@@ -19,11 +19,23 @@ db_45 = df_45.iloc[1:,[1]]
 db_90 = df_90.iloc[1:,[1]]
 
 # Suavizado con función externa
-suavizado_0 = suavizado(df_0['Unnamed: 0'], df_0['rta freq 0°'], 3)
+suavizado_0 = suavizado(df_0['Frequency (Hz)'], df_0['Magnitude (dB)'], 3)
+suavizado_45 = suavizado(df_45['Frequency (Hz)'], df_45['Magnitude (dB)'], 3)
+suavizado_90 = suavizado(df_90['Frequency (Hz)'], df_90['Magnitude (dB)'], 3)
+
+# Traspaso de Dataframe a ndarray para que 
+# data de magnitud y frecuencia matcheen 
+
+frec_0 = frec_0.to_numpy()
+frec_45 = frec_45.to_numpy()
+frec_90 = frec_90.to_numpy()
+suavizado_0 = suavizado_0[1:]
+suavizado_45 = suavizado_45[1:]
+suavizado_90 = suavizado_90[1:]
 
 #Plot Rta. Fcia. 0°
 plt.figure(1)
-plt.plot(frec_0,db_0)
+plt.plot(frec_0,suavizado_0)
 plt.semilogx()
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude [dB]")
@@ -32,7 +44,7 @@ plt.ylim([-2,9])
 
 #Plot Rta. Fcia. 45°
 plt.figure(2)
-plt.plot(frec_45,db_45)
+plt.plot(frec_45,suavizado_45)
 plt.semilogx()
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude [dB]")
@@ -41,7 +53,7 @@ plt.ylim([-2,9])
 
 #Plot Rta. Fcia. 90°
 plt.figure(3)
-plt.plot(frec_90,db_90)
+plt.plot(frec_90,suavizado_90)
 plt.semilogx()
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude [dB]")
